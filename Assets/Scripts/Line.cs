e47e388a6f15ab684cfce358764d29b03abb582a
nghia151 <nghia151@gmail.com>
2023-06-27 20:55:24 +0700

    aaa

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public float moveSpeed;
+    float xDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xDirection = Input.GetAxis("Horizontal");
+
+        float moveStep = moveSpeed * xDirection * Time.deltaTime;
+        transform.position = transform.position + new Vector3(moveStep, 0, 0);
+    }
+}
